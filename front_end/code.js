caa9bcba306f34405bfe54538dbb88bb1de348da
Gustavo <crymerom@gmail.com>
2024-04-23 22:49:49 +0100

    feat: report phishing modal

--- front_end/code.js
+++ front_end/code.js
@@ -10,6 +10,15 @@ function showSidebar() {
 }
 
 function openSettings() {
-  var html = HtmlService.createTemplateFromFile('settings').evaluate().setTitle("Settings")
+  var html = HtmlService.createTemplateFromFile('settings').evaluate().setTitle("Settings");
   DocumentApp.getUi().showSidebar(html);
+}
+
+function openReportPhishingModal() {
+  var html = HtmlService.createTemplateFromFile('reportPhishing').evaluate().setTitle("Report Phishing");
+  DocumentApp.getUi().showModalDialog(html, 'Report Phishing');
+}
+
+function processAttackerEmail(attackerEmail) {
+  Logger.log('Attacker\'s Email:', attackerEmail);
 }
