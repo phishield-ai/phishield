e8d25cec3cc56cd854c1d206f9f422f6a6fb13cc
Farley <kfbfarley@gmail.com>
2024-05-01 20:01:21 +0000

    Add docstring to liveness and include routes in docs

--- backend/phishield/packages/fastapi/healthcheks/routes.py
+++ backend/phishield/packages/fastapi/healthcheks/routes.py
@@ -1,12 +1,26 @@
 from fastapi import APIRouter
+from pydantic import BaseModel
 
+
+
+class APIStatus(BaseModel):
+    status: str
 
 router = APIRouter(
     tags=["API Healthchecks"],
-    include_in_schema=False,
+    include_in_schema=True
 )
 
-
-@router.get("/healthz")
+@router.get(
+    "/healthz",
+    summary="Health",
+    response_model=APIStatus,
+)
 async def liveness():
-    return {"status": "OK"}
+    """
+    Endpoint for checking the liveness of the API.
+    
+    Returns:
+        APIStatus: An instance of APIStatus containing the status of the API.
+    """
+    return APIStatus(status="OK")
